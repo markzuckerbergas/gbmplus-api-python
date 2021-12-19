53350a33b10fc64dd83d0c663aee8a34b50e1304
markzuckerbergas <markzuckerbergas@protonmail.com>
2021-12-19 17:54:26 -0600

    Added __name__ == '__main__'

--- examples/transfer.py
+++ examples/transfer.py
@@ -1,20 +1,28 @@
+import gbmplus
+from datetime import datetime
+
 # Transfer from the Smart Cash strategy to a target strategy
 
-import gbmplus
+def main():    
+    gbm = gbmplus.GBMPlusAPI(output_log=False)
 
-gbm = gbmplus.GBMPlusAPI(output_log=False)
+    accounts = gbm.accounts.getAccounts()
+    accounts_dict = {element["name"]: element for element in accounts}
 
-accounts = gbm.accounts.getAccounts()
-accounts_dict = {element["name"]: element for element in accounts}
+    smart_cash_account = accounts_dict.get('Smart Cash')
+    target_account = accounts_dict.get('NAME OF YOUR STRATEGY')
 
-smart_cash_account = accounts_dict.get('Smart Cash')
-target_account = accounts_dict.get('NAME OF YOUR STRATEGY')
+    if smart_cash_account and target_account:    
+        response = gbm.transfers.transfer(
+            amount=100, # in MXN
+            origin_account_id=smart_cash_account.get('account_id'),
+            target_account_id=target_account.get('account_id')
+        )
 
-if smart_cash_account and target_account:    
-    response = gbm.transfers.transfer(
-        amount=100, # in MXN
-        origin_account_id=smart_cash_account.get('account_id'),
-        target_account_id=target_account.get('account_id')
-    )
-
-    print(response)
+        print(response)
+        
+if __name__ == '__main__':
+    start_time = datetime.now()
+    main()
+    end_time = datetime.now()
+    print(f'\nScript complete, total runtime {end_time - start_time}')
